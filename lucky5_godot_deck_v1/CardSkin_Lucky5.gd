c799c4efd379a57ea56eb2920a2d6aaf8df4cdf1
ironkid90 <ironkid90@gmail.com>
2026-05-09 23:28:58 +0300

    Revert "c"
    This reverts commit 62f5b4b92d0110f49725937ff2b26ca2683a199a.

---
+++ lucky5_godot_deck_v1/CardSkin_Lucky5.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const CARD_BASE_PATH := "res://skins/lucky5/cards/"
+const BACK := CARD_BASE_PATH + "bside.png"
+const HOLD_BACK := CARD_BASE_PATH + "holdbside.png"
+
+static func card_path(rank: String, suit: String) -> String:
+	# rank: "2".."10", "J", "Q", "K", "A"; suit: "C", "D", "H", "S"
+	return CARD_BASE_PATH + rank + suit + ".png"
+
+static func card_texture(rank: String, suit: String) -> Texture2D:
+	return load(card_path(rank, suit)) as Texture2D
